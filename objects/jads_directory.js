119761aa98db6a93bb4ec648239980b8799788c1
Brenton O'Callaghan <Callaghan001@gmail.com>
2014-01-15 20:35:55 +0000

    Tidy up of directory listing as well as indication as to which entries are files and which are dirs.

--- objects/jads_directory.js
+++ objects/jads_directory.js
@@ -2,19 +2,12 @@ var gc = require('../config.js');       // Global Variables.
 var err = require(gc.error_object);     // JADS error object.
 var fs    = require('fs');              // Node file system object.
 
-// We read in a template file and then do a find and replace for specifc strings
-// in order to format the file.
-exports.readDirectoryListTemplateFile = function () {
-    
-    
-};
-
 exports.returnDirectoryContentsForPath = function (fileSystemPath, httpResponse, requestURL) {
     var i;  // Loop variable used during directory contents looping.
     
 	fs.readdir(fileSystemPath, function (error, files) {
 
-		var responseBody, responseString = '<table>';
+		var responseBody, fileType, fileStats, responseStringFiles = '<table class="files">', responseStringDirs = '<table class="dirs">';
         
         responseBody = fs.readFileSync('./objects/jads_dir_list.template', {encoding : 'utf-8'});
 
@@ -23,13 +16,25 @@ exports.returnDirectoryContentsForPath = function (fileSystemPath, httpResponse,
 
 		} else {
 			for (i = files.length - 1; i >= 0; i = i - 1) {
+                
+                fileStats = fs.statSync(fileSystemPath + files[i]);
+                
+                if (!fileStats.isFile()) {
+                    fileType = 'Dir';
+                    responseStringDirs = responseStringDirs + '<tr><td>' + fileType + '</td><td><a href="' + gc.coreFunctions.joinPaths(requestURL, files[i]) + '">' + files[i] + '</td></tr></li>';
 
-				responseString = responseString + '<tr><td><a href="' + gc.coreFunctions.joinPaths(requestURL, files[i]) + '">' + files[i] + '</td></tr></li>';
+                } else {
+                    fileType = 'File';
+                    responseStringFiles = responseStringFiles + '<tr><td>' + fileType + '</td><td><a href="' + gc.coreFunctions.joinPaths(requestURL, files[i]) + '">' + files[i] + '</td></tr></li>';
+
+                }
+
 			}
-			responseString = responseString + '</table>';
+			responseStringFiles = responseStringFiles + '</table>';
+            responseStringDirs = responseStringDirs + '</table>';
 			httpResponse.setHeader("Content-Type", 'text/html');
             
-            responseBody = responseBody.replace('{dirContents}', responseString);
+            responseBody = responseBody.replace('{dirContents}', responseStringDirs + responseStringFiles);
             responseBody = responseBody.replace(new RegExp('{dirName}', 'g'), requestURL);
 
 			// Write the response code
