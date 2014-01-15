9c06b0a124972d5503825e9bb2fba4e472ee81fe
Brenton O'Callaghan <Callaghan001@gmail.com>
2014-01-15 20:07:18 +0000

    Addition of a basic template for when displaying a directory listing. Next step - distinguish between a file and a directory

--- objects/jads_directory.js
+++ objects/jads_directory.js
@@ -2,12 +2,21 @@ var gc = require('../config.js');       // Global Variables.
 var err = require(gc.error_object);     // JADS error object.
 var fs    = require('fs');              // Node file system object.
 
+// We read in a template file and then do a find and replace for specifc strings
+// in order to format the file.
+exports.readDirectoryListTemplateFile = function () {
+    
+    
+};
+
 exports.returnDirectoryContentsForPath = function (fileSystemPath, httpResponse, requestURL) {
     var i;  // Loop variable used during directory contents looping.
     
 	fs.readdir(fileSystemPath, function (error, files) {
 
-		var responseString = '<html><head></head><body><table>';
+		var responseBody, responseString = '<table>';
+        
+        responseBody = fs.readFileSync('./objects/jads_dir_list.template', {encoding : 'utf-8'});
 
 		if (error) {
 			err.newError(500, 'Unable to complete request - server dir unreadable ', undefined, httpResponse, 'jad_directory.js', 'returnDirectoryContentsForPath');
@@ -17,12 +26,15 @@ exports.returnDirectoryContentsForPath = function (fileSystemPath, httpResponse,
 
 				responseString = responseString + '<tr><td><a href="' + gc.coreFunctions.joinPaths(requestURL, files[i]) + '">' + files[i] + '</td></tr></li>';
 			}
-			responseString = responseString + '</table></body></html>';
+			responseString = responseString + '</table>';
 			httpResponse.setHeader("Content-Type", 'text/html');
+            
+            responseBody = responseBody.replace('{dirContents}', responseString);
+            responseBody = responseBody.replace(new RegExp('{dirName}', 'g'), requestURL);
 
 			// Write the response code
             httpResponse.writeHead(200);
-            httpResponse.end(responseString);
+            httpResponse.end(responseBody);
 		}
 
 	});
